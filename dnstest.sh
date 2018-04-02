f499b92c911a44b29ec37aa9a829cf7acfe35231
Joel Schopp <joel.schopp@gmail.com>
2018-04-02 15:03:05 -0500

    Behave better with timeouts
    Crurently the script behaved poorly with timeouts.  Make it behave better.  Also add a couple of secondary dns entries to test.

--- dnstest.sh
+++ dnstest.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+
 PROVIDERS="
 1.1.1.1#cloudflare 
+1.0.0.1#cloudflare2nd 
 8.8.8.8#google 
+8.8.4.4#google2nd
 9.9.9.9#quad9 
 208.67.222.123#opendns 
 199.85.126.20#norton 
@@ -20,7 +23,7 @@ DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.
 totaldomains=0
 printf "%-15s" ""
 for d in $DOMAINS2TEST; do
-    totaldomains=`expr $totaldomains + 1`
+    totaldomains=$((totaldomains + 1))
     printf "%-8s" "test$totaldomains"
 done
 printf "%-8s" "Average"
@@ -34,9 +37,13 @@ for p in $PROVIDERS; do
 
     printf "%-15s" "$pname"
     for d in $DOMAINS2TEST; do
-        ttime=`dig $d @$pip |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
+        ttime=`dig @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
+	if [ -z "$ttime" ]; then
+	    #let's have time out be 1s = 1000ms
+	    ttime=1000
+	fi
         printf "%-8s" "$ttime ms"
-        ftime=`expr $ftime + $ttime`
+        ftime=$((ftime + ttime))
     done
     avg=`bc <<< "scale=2; $ftime/$totaldomains"`
 

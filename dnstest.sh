276bd74459463c1817325aaa66e6be27dbd0e0a6
CleanBrowsing <36643808+cleanbrowsing@users.noreply.github.com>
2018-04-03 09:29:45 -0700

    Adding support for the current nameservers
    Adding the current nameservers from /etc/resolv.conf to be tested.

--- dnstest.sh
+++ dnstest.sh
@@ -3,11 +3,13 @@
 command -v bc > /dev/null || { echo "bc was not found. Please install bc."; exit 1; }
 { command -v drill > /dev/null && dig=drill; } || { command -v dig > /dev/null && dig=dig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
 
+
+
+NAMESERVERS=`cat /etc/resolv.conf |grep nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
+
 PROVIDERS="
 1.1.1.1#cloudflare 
-1.0.0.1#cloudflare2nd 
 8.8.8.8#google 
-8.8.4.4#google2nd
 9.9.9.9#quad9 
 208.67.222.123#opendns 
 199.85.126.20#norton 
@@ -23,7 +25,7 @@ DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.
 
 
 totaldomains=0
-printf "%-15s" ""
+printf "%-18s" ""
 for d in $DOMAINS2TEST; do
     totaldomains=$((totaldomains + 1))
     printf "%-8s" "test$totaldomains"
@@ -32,18 +34,21 @@ printf "%-8s" "Average"
 echo ""
 
 
-for p in $PROVIDERS; do
+for p in $NAMESERVERS $PROVIDERS; do
     pip=${p%%#*}
     pname=${p##*#}
     ftime=0
 
-    printf "%-15s" "$pname"
+    printf "%-18s" "$pname"
     for d in $DOMAINS2TEST; do
-        ttime=`$dig +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
-	if [ -z "$ttime" ]; then
-	    #let's have time out be 1s = 1000ms
-	    ttime=1000
-	fi
+        ttime=`$dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
+        if [ -z "$ttime" ]; then
+	        #let's have time out be 1s = 1000ms
+	        ttime=1000
+        elif [ "x$ttime" = "x0" ]; then
+	        ttime=1
+	    fi
+
         printf "%-8s" "$ttime ms"
         ftime=$((ftime + ttime))
     done

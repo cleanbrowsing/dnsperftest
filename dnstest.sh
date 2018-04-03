e6512ed001e0150ffb7623ed91cc67ba312e902b
Timothy Sedlmeyer <tim@sedlmeyer.org>
2018-04-03 12:51:53 -0400

    Clean up formatting

--- dnstest.sh
+++ dnstest.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-command -v bc > /dev/null || { echo "bc was not found. Please install bc."; exit 1; }
-{ command -v drill > /dev/null && dig=drill; } || { command -v dig > /dev/null && dig=dig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
+command -v bc >/dev/null || {
+    echo "bc was not found. Please install bc."
+    exit 1
+}
+{ command -v drill >/dev/null && dig=drill; } || { command -v dig >/dev/null && dig=dig; } || {
+    echo "dig was not found. Please install dnsutils."
+    exit 1
+}
 
-NAMESERVERS=$(cat /etc/resolv.conf |grep nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/')
+NAMESERVERS=$(cat /etc/resolv.conf | grep nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/')
 
 PROVIDERS="
 1.1.1.1#cloudflare 
@@ -39,21 +45,20 @@ for p in $NAMESERVERS $PROVIDERS; do
 
     printf "%-18s" "$pname"
     for d in $DOMAINS2TEST; do
-        ttime=$("$dig" +tries=1 +time=2 +stats @"$pip" "$d" |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2)
-    	if [ -z "$ttime" ]; then
-			#let's have time out be 1s = 1000ms
-			ttime=1000
+        ttime=$("$dig" +tries=1 +time=2 +stats @"$pip" "$d" | grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2)
+        if [ -z "$ttime" ]; then
+            #let's have time out be 1s = 1000ms
+            ttime=1000
         elif [ "x$ttime" = "x0" ]; then
-			ttime=1
-		fi
+            ttime=1
+        fi
 
         printf "%-8s" "$ttime ms"
         ftime=$((ftime + ttime))
     done
-    avg=$(bc -lq <<< "scale=2; $ftime/$totaldomains")
+    avg=$(bc -lq <<<"scale=2; $ftime/$totaldomains")
 
     echo "  $avg"
 done
 
-
-exit 0;
+exit 0

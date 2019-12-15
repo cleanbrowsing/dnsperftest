6b59e1e5066c7f263f383d0448a6616e85ce0918
graysky <graysky@archlinux.us>
2019-12-15 07:18:47 -0500

    Improve grammar and robustness, see shellcheck.net

--- dnstest.sh
+++ dnstest.sh
@@ -4,31 +4,32 @@ command -v bc > /dev/null || { echo "bc was not found. Please install bc."; exit
 { command -v drill > /dev/null && dig=drill; } || { command -v dig > /dev/null && dig=dig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
 
 
-
-NAMESERVERS=`cat /etc/resolv.conf | grep ^nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
-
-PROVIDERS="
-1.1.1.1#cloudflare 
-4.2.2.1#level3 
-8.8.8.8#google 
-9.9.9.9#quad9 
-80.80.80.80#freenom 
-208.67.222.123#opendns 
-199.85.126.20#norton 
-185.228.168.168#cleanbrowsing 
-77.88.8.7#yandex 
-176.103.130.132#adguard 
-156.154.70.3#neustar 
-8.26.56.26#comodo
-"
+mapfile -t PROVIDERS< <(grep -r '^nameserver' /etc/resolv.conf | cut -d " " -f 2 | sed 's/\(.*\)/&#&/')
+PROVIDERS+=(
+  1.1.1.1#cloudflare
+  4.2.2.1#level3
+  8.8.8.8#google
+  9.9.9.9#quad9
+  80.80.80.80#freenom
+  208.67.222.123#opendns
+  199.85.126.20#norton
+  185.228.168.168#cleanbrowsing
+  77.88.8.7#yandex
+  176.103.130.132#adguard
+  156.154.70.3#neustar
+  8.26.56.26#comodo
+)
 
 # Domains to test. Duplicated domains are ok
-DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com"
+DOMAINS2TEST=( 
+  www.google.com amazon.com facebook.com www.youtube.com www.reddit.com
+  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com
+)
 
 
 totaldomains=0
 printf "%-18s" ""
-for d in $DOMAINS2TEST; do
+for d in "${DOMAINS2TEST[@]}"; do
     totaldomains=$((totaldomains + 1))
     printf "%-8s" "test$totaldomains"
 done
@@ -36,25 +37,25 @@ printf "%-8s" "Average"
 echo ""
 
 
-for p in $NAMESERVERS $PROVIDERS; do
+for p in "${PROVIDERS[@]}"; do
     pip=${p%%#*}
     pname=${p##*#}
     ftime=0
 
     printf "%-18s" "$pname"
-    for d in $DOMAINS2TEST; do
-        ttime=`$dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
+    for d in "${DOMAINS2TEST[@]}"; do
+        ttime=$(dig +tries=1 +time=2 +stats @"$pip" "$d" |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2)
         if [ -z "$ttime" ]; then
-	        #let's have time out be 1s = 1000ms
-	        ttime=1000
+          #let's have time out be 1s = 1000ms
+          ttime=1000
         elif [ "x$ttime" = "x0" ]; then
-	        ttime=1
-	    fi
+          ttime=1
+      fi
 
         printf "%-8s" "$ttime ms"
         ftime=$((ftime + ttime))
     done
-    avg=`bc -lq <<< "scale=2; $ftime/$totaldomains"`
+    avg=$(bc -lq <<< "scale=2; $ftime/$totaldomains")
 
     echo "  $avg"
 done

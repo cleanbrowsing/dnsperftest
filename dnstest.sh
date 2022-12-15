8bddbebab4cf1b4cb50d5c002e05f1838703a25c
Micheal Espinola <michealespinola@users.noreply.github.com>
2022-12-14 16:31:53 -0800

    major changes for compatibility and usability
    1. Removed `bc` requirement and use `awk` instead, for systems that do not have `bc` for integer maths
    1. Revamp how arrays are built and sanitized
    1. Automatically detect and test against self-hosted DNS (checks for port 53 use and adds 127.0.0.1 to the nameserver list)
    1. Auto-number resolv.conf nameservers to distinguish listings
    1. Revamp internal DNS Provider and Domains 2 Test lists, compensating for bad entries and adding the ability to add comments lines
    1. Modify logging to be as compact as reasonably possible. Providers column is now dynamically sized
    1. Display corresponding Domains 2 Test list at end so we know which tests were to what

--- dnstest.sh
+++ dnstest.sh
@@ -1,104 +1,296 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2034,SC2207
+# shellcheck source=/dev/null
+# bash /volume1/homes/admin/scripts/bash/dnstest.sh
 
+# CHECK FOR DIG REQUIREMENT
+if ! command -v dig &> /dev/null; then
+  echo
+  echo "* DIG (Domain Information Gopher) is required for DNS queries, but"
+  echo "  is not found in the PATH. Please install it."
+  echo
+  if [ -e "/proc/sys/kernel/syno_hw_version" ]; then
+    echo "  DIG can be installed as a part of the 'SynoCli Network Tools'"
+    echo "  package. This can be installed via the DSM Package Center by"
+    echo "  adding the 'SynoCommunity' Package Source site, located at:"
+    echo
+    echo "  https://packages.synocommunity.com/"
+  else
+    echo "  DIG can typically be installed as a part of the 'dnsutils'"
+    echo "  or 'bind-utils' packages. These can be installed via your"
+    echo "  package management system."
+  fi
+    echo
+    echo Exiting...
+    echo
+exit 1
+fi
 
-command -v bc > /dev/null || { echo "error: bc was not found. Please install bc."; exit 1; }
-{ command -v drill > /dev/null && dig=drill; } || { command -v dig > /dev/null && dig=dig; } || { echo "error: dig was not found. Please install dnsutils."; exit 1; }
+# BUILD LIST OF NAMESERVERS USED BY THIS HOST
+NAMESERVERS=($(grep ^nameserver /etc/resolv.conf | awk 'BEGIN{i=0}{i++;}{print $2 "#" $1 "/" i}'))
 
+# CHECK IF LOCALHOST IS RUNNING DNS ON THE DEFAULT PORT
+if LOCALTEST=$(netstat -tulpnW | grep "\:53\b" | grep "tcp\b" | grep "LISTEN"); then
+# if [ "$?" -eq "0" ]; then
+  LOCALDNS=$(echo "$LOCALTEST" | awk -F '/' 'NR==1{ print $2 }')
+  LOCALHOST=127.0.0.1#"$LOCALDNS"
+  NAMESERVERS=("${LOCALHOST}" "${NAMESERVERS[@]}")
+fi
+# SANITIZE NAMESERVERS ARRAY
+shopt -s extglob                                       # turn on extended glob
+NAMESERVERS=( "${NAMESERVERS[@]/#+([[:blank:]])/}" )   # remove leading space/tab from each element
+NAMESERVERS=( "${NAMESERVERS[@]/%+([[:blank:]])/}" )   # remove trailing space/tab from each element
 
-NAMESERVERS=`cat /etc/resolv.conf | grep ^nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
-
+# BUILD LIST OF IPV4 DNS PROVIDERS
 PROVIDERSV4="
-1.1.1.1#cloudflare 
-4.2.2.1#level3 
-8.8.8.8#google 
-9.9.9.9#quad9 
-80.80.80.80#freenom 
-208.67.222.123#opendns 
-199.85.126.20#norton 
-185.228.168.168#cleanbrowsing 
-77.88.8.7#yandex 
-176.103.130.132#adguard 
-156.154.70.3#neustar 
-8.26.56.26#comodo
-45.90.28.202#nextdns
-"
+# NOTABLE IP4 DNS PROVIDERS
+    94.140.14.14#AdGuard/1 
+    94.140.15.15#AdGuard/2
+   185.228.168.9#CleanBrowsing/1 
+   185.228.169.9#CleanBrowsing/2 
+         1.1.1.1#Cloudflare/1
+         1.0.0.1#Cloudflare/2
+      8.26.56.26#Comodo/1
+     8.20.247.20#Comodo/2
+    84.200.69.80#DNS.Watch/1
+    84.200.70.40#DNS.Watch/2
+         8.8.8.8#Google/1 
+         8.8.4.4#Google/2 
+         4.2.2.1#Level3/1 
+         4.2.2.2#Level3/2 
+       64.6.64.6#Neustar/1
+       64.6.65.6#Neustar/2
+  208.67.222.222#OpenDNS/1
+  208.67.220.220#OpenDNS/2
+   216.146.35.35#OracleDyn/1
+   216.146.36.36#OracleDyn/2
+         9.9.9.9#Quad9/1 
+ 149.112.112.112#Quad9/2
 
+# OTHER IP4 DNS PROVIDERS
+     76.76.19.19#AlternateDNS/1
+  76.223.122.150#AlternateDNS/2
+       76.76.2.0#ControlD/1
+      76.76.10.0#ControlD/2
+   103.247.36.36#DNSFilter/1
+   103.247.37.37#DNSFilter/2
+     80.80.80.80#Freenom/1
+     80.80.81.81#Freenom/2
+    45.90.28.243#NextDNS/1
+    45.90.30.243#NextDNS/2
+   199.85.126.10#NortonCS/1 
+   199.85.127.10#NortonCS/2 
+    195.46.39.39#SafeDNS/1
+    195.46.39.40#SafeDNS/2
+       64.6.64.6#Verisign/1
+       64.6.65.6#Verisign/2
+       77.88.8.8#Yandex/1 
+       77.88.8.1#Yandex/2
+"
+# SORT PROVIDERSV4 ARRAY
+IFS=$'\n' PROVIDERSV4=($(sort -t "#" -k 2,2 <<<"${PROVIDERSV4[@]}")); unset IFS
+
+# BUILD LIST OF IPV6 DNS PROVIDERS
 PROVIDERSV6="
-2606:4700:4700::1111#cloudflare-v6
-2001:4860:4860::8888#google-v6
-2620:fe::fe#quad9-v6
-2620:119:35::35#opendns-v6
-2a0d:2a00:1::1#cleanbrowsing-v6
-2a02:6b8::feed:0ff#yandex-v6
-2a00:5a60::ad1:0ff#adguard-v6
-2610:a1:1018::3#neustar-v6
+# NOTABLE IP6 DNS PROVIDERS
+         2a10:50c0::ad1:ff#AdGuard/1-v6
+         2a10:50c0::ad2:ff#AdGuard/2-v6
+            2a0d:2a00:1::2#CleanBrowsing/1-v6
+            2a0d:2a00:2::2#CleanBrowsing/2-v6
+      2606:4700:4700::1111#Cloudflare/1-v6
+      2606:4700:4700::1001#Cloudflare/2-v6
+2001:1608:10:25::1c04:b12f#DNS.Watch/1
+2001:1608:10:25::9249:d69b#DNS.Watch/2
+      2001:4860:4860::8888#Google/1-v6
+      2001:4860:4860::8844#Google/2-v6
+           2620:74:1b::1:1#Neustar/1
+           2620:74:1c::2:2#Neustar/2
+           2620:119:35::35#OpenDNS/1-v6
+           2620:119:53::53#OpenDNS/2-v6
+               2620:fe::fe#Quad9/1-v6
+                2620:fe::9#Quad9/2-v6
+
+# OTHER IP6 DNS PROVIDERS
+             2602:fcbc::ad#AlternateDNS/1-v6
+           2602:fcbc:2::ad#AlternateDNS/2-v6
+               2606:1a40::#ControlD/1-v6
+             2606:1a40:1::#ControlD/2-v6
+        2a07:a8c0::11:b227#NextDNS/1-v6
+        2a07:a8c1::11:b227#NextDNS/2-v6
+       2001:67c:2778::3939#SafeDNS/1-v6
+       2001:67c:2778::3939#SafeDNS/2-v6
+        2a02:6b8::feed:0ff#Yandex/1-v6 
+    2a02:6b8:0:1::feed:0ff#Yandex/2-v6
 "
+# SORT PROVIDERSV6 ARRAY
+IFS=$'\n' PROVIDERSV6=($(sort -t "#" -k 2,2 <<<"${PROVIDERSV6[@]}")); unset IFS
 
-# Testing for IPv6
-$dig +short +tries=1 +time=2 +stats @2a0d:2a00:1::1 www.google.com |grep 216.239.38.120 >/dev/null 2>&1
-if [ $? = 0 ]; then
-    hasipv6="true"
-fi
+# BUILD LIST OF DOMAINS TO TEST AGAINST
+DOMAINS2TEST="
+# TOP 9 WEBSITES 2022
+  www.google.com
+  www.youtube.com
+  www.facebook.com
+  www.amazon.com
+  www.reddit.com
+  www.apple.com
+  www.pornhub.com
+  www.yahoo.com
+  www.wikipedia.org
 
-providerstotest=$PROVIDERSV4
+# OTHER TOP WEBSITES 2022
+# www.twitter.com
+# www.paypal.com
+# www.xvideos.com
+# www.instagram.com
+# www.bing.com
+# www.fandom.com
+# www.microsoftonline.com
+# www.walmart.com
+# www.spankbang.com
+# www.twitch.tv
+# www.tiktok.com
+# www.linkedin.com
+# www.duckduckgo.com
+# www.weather.com
+# www.indeed.com
+# www.qccerttest.com
+# www.quora.com
+# www.ebay.com
+# www.xnxx.com
+# www.cnn.com
+# www.espn.com
+# www.xhamster.com
+# www.etsy.com
+# www.nytimes.com
+# www.imdb.com
+# www.redd.it
+# www.usps.com
+# www.office.com
+# www.microsoft.com
+# www.zillow.com
+# www.live.com
+"
+# SORT DOMAINS2TEST ARRAY
+IFS=$'\n' DOMAINS2TEST=($(sort -u <<<"${DOMAINS2TEST[@]}")); unset IFS
 
-if [ "x$1" = "xipv6" ]; then
-    if [ "x$hasipv6" = "x" ]; then
-        echo "error: IPv6 support not found. Unable to do the ipv6 test."; exit 1;
-    fi
-    providerstotest=$PROVIDERSV6
+# SANITIZE DOMAINS2TEST ARRAY
+shopt -s extglob                                               # turn on extended glob
+DOMAINS2TEST=( "${DOMAINS2TEST[@]/#+([[:blank:]])/}" )   # remove leading space/tab from each element
+DOMAINS2TEST=( "${DOMAINS2TEST[@]/%+([[:blank:]])/}" )   # remove trailing space/tab from each element
 
-elif [ "x$1" = "xipv4" ]; then
-    providerstotest=$PROVIDERSV4
-
-elif [ "x$1" = "xall" ]; then
-    if [ "x$hasipv6" = "x" ]; then
-        providerstotest=$PROVIDERSV4
-    else
-        providerstotest="$PROVIDERSV4 $PROVIDERSV6"
-    fi
+if [ "$1" = "ipv6" ]; then
+  providerstotest=("${PROVIDERSV6[@]}")
+elif [ "$1" = "ipv4" ]; then
+  providerstotest=("${PROVIDERSV4[@]}")
+elif [ "$1" = "all" ]; then
+    providerstotest=("${PROVIDERSV4[@]}" "${PROVIDERSV6[@]}")
 else
-    providerstotest=$PROVIDERSV4
+  providerstotest=("${PROVIDERSV4[@]}")
 fi
+if [ "$1" = "host" ]; then
+  providerstotest=("${NAMESERVERS[@]}")
+elif [ "$1" = "-host" ]; then
+  providerstotest=("${providerstotest[@]}")
+else
+  providerstotest=("${NAMESERVERS[@]}" "${providerstotest[@]}")
+fi
+# SANITIZE NAMESERVERS ARRAY
+shopt -s extglob                                               # turn on extended glob
+providerstotest=( "${providerstotest[@]/#+([[:blank:]])/}" )   # remove leading space/tab from each element
+providerstotest=( "${providerstotest[@]/%+([[:blank:]])/}" )   # remove trailing space/tab from each element
 
-    
-
-# Domains to test. Duplicated domains are ok
-DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com"
-
-
-totaldomains=0
-printf "%-21s" ""
-for d in $DOMAINS2TEST; do
-    totaldomains=$((totaldomains + 1))
-    printf "%-8s" "test$totaldomains"
+# DETERMINE NAMESERVERS NAME LENGTH ($nsl)
+for item in "${providerstotest[@]}"; do
+if [[ -n "$item" ]] && [[ ! $item =~ ^#.* ]]; then
+  pname=${item##*#}
+  plength[${#pname}]=${item##*#} # use word length as index
+fi
 done
-printf "%-8s" "Average"
-echo ""
+longest=("${plength[@]: -1}")    # select last array element
+length=$((${#longest}))
+nsl="%-$((${#longest} + 1))s"
 
+echo 
+totaldomains=0
+eval printf '$nsl' "Provider"
+for d in "${DOMAINS2TEST[@]}"; do
+if [[ -n "$d" ]] && [[ ! $d =~ ^#.* ]]; then
+  totaldomains=$((totaldomains + 1))
+  printf "%-4s" "t$totaldomains"
+fi
+done
+printf "%9s\n" "Median ms"
+eval printf -- '-%.0s' "{1..$length}"
+printf ' %.0s' ""
+for d in "${DOMAINS2TEST[@]}"; do
+if [[ -n "$d" ]] && [[ ! $d =~ ^#.* ]]; then
+  printf "%-4s" "---"
+fi
+done
+printf "%-4s\n" "---------"
 
-for p in $NAMESERVERS $providerstotest; do
+for p in "${providerstotest[@]}"; do
+if [[ -n "$p" ]] && [[ ! $p =~ ^#.* ]]; then
+
     pip=${p%%#*}
     pname=${p##*#}
     ftime=0
+    xtime=0
 
-    printf "%-21s" "$pname"
-    for d in $DOMAINS2TEST; do
-        ttime=`$dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
-        if [ -z "$ttime" ]; then
-	        #let's have time out be 1s = 1000ms
-	        ttime=1000
-        elif [ "x$ttime" = "x0" ]; then
-	        ttime=1
-	    fi
+    eval printf '$nsl' "$pname"
+    for d in "${DOMAINS2TEST[@]}"; do
+    if [[ -n "$d" ]] && [[ ! $d =~ ^#.* ]]; then
 
-        printf "%-8s" "$ttime ms"
-        ftime=$((ftime + ttime))
+      ttime=$(dig +tries=1 +timeout=1  @"$pip" "$d" |  grep "Query time:" | awk '{ print $4 }')
+
+
+  # NSLOOKUP INSTEAD OF DIG
+      # TEST=$((time (nslookup -timeout=1 -retry=0 www.microsoft.com 89.233.43.71)) 2>&1)
+      # echo "$TEST" | grep "connection timed out"
+      # echo "$TEST" | grep "real"
+
+
+
+      if [ -z "$ttime" ]; then
+  	    #let's have time out be 1s = 1000ms
+  	    ttime=1000
+      elif [ "$ttime" = "0" ]; then
+  	    ttime=1
+  	  fi
+
+      if [ "$ttime" -ge "1000" ]; then
+       printf "%-4s" "*"
+       xtime=$(( xtime+1 ))
+      else
+        printf "%-4s" "$ttime"
+      fi
+
+      ftime=$((ftime + ttime))
+
+    fi
     done
-    avg=`bc -l <<< "scale=2; $ftime/$totaldomains"`
+    avg=$(awk -v a="$ftime" -v b="$totaldomains" 'BEGIN { printf "%.2f", a/b }' </dev/null)
 
-    echo "  $avg"
+      if [ "$xtime" -gt "0" ]; then
+        printf "%9s\n" "$xtime t/o"
+        unset xtime
+      else
+        printf "%9s\n" "$avg ms"
+      fi
+  # sleep 30
+  fi
 done
 
+echo 
+unset totaldomains
+for d in "${DOMAINS2TEST[@]}"; do
+if [[ -n "$d" ]] && [[ ! $d =~ ^#.* ]]; then
 
-exit 0;
+  totaldomains=$((totaldomains + 1))
+  printf "%3s: %s\n" "t$totaldomains" "$d"
+fi
+done
+
+echo
+# exit 0;
